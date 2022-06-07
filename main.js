9fb55bf3ff01b6b4f057ebc0a094e2137720b53c
Rumidich <rumidich@gmail.com>
2022-06-07 11:39:43 +0600

    added post request

--- main.js
+++ main.js
@@ -9,3 +9,25 @@ GET - получение данных
 команда для запуска json-server
 json-server -w db.json -p 8000
 */
+/*
+CRUD - Create(POST-request) Read(GET-requests) Update(PUT/PATCH-requests) Delete(DELETE-requests)
+*/
+
+const API = "http://localhost:8000/todos";
+
+let inpAdd = document.getElementById("inp-add");
+let btnAdd = document.getElementById("btn-add");
+// console.log(inpAdd, btnAdd);
+btnAdd.addEventListener("click", function () {
+  let newTodo = {
+    todo: inpAdd.value,
+  };
+  // console.log(newTodo);
+  fetch(API, {
+    method: "POST",
+    body: JSON.stringify(newTodo),
+    headers: {
+      "Content-type": "application/json; charset=utf-8",
+    },
+  });
+});
